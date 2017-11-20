69cea502b8f679528cbb92131a62830dd842743b
Mrityunjoy Chowdhury <33812263+mrityunia@users.noreply.github.com>
2017-11-20 23:19:29 +0530

    maven update
    Updated maven project

--- outlook/src/main/java/com/common/Base.java
+++ outlook/src/main/java/com/common/Base.java
@@ -4,18 +4,16 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
 import java.util.Properties;
 
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
 import com.utility.Constraints;
 
 public class Base {
